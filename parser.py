7f6dd11c429682a8be5a0c4f3d9c410787cf7c63
Daniel Leyva <leyva.danielr@gmail.com>
2016-09-26 17:06:18 -0500

    parser function added in file parser.py

---
+++ parser.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+
+def parser( fileToParse, lineToParse, stringToParse):
+	stringToParse = stringToParse.lower()
+	stringToParse = stringToParse.replace(' ','')
+	stringToParse = stringToParse.replace('\n', '')
+	f = open(fileToParse)
+	txt = f.readlines()
+	x = txt[lineToParse]
+	x = x.lower()
+	x = x.replace('\n', '')
+	x = x.replace(' ','')
+	print x
+	print stringToParse
+	if x == stringToParse:
+		return True
+	else:
+		return False
