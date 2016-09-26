4f5045643017e3d14ff57d52efc3de6fe3cd3ddb
Juan Nolazco <carlos.bcaceres@hotmail.com>
2016-09-26 16:34:10 -0500

    branch prueba

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
- # -*- coding: utf-8 -*-
+import sys
+import time
+
+IPC = "10.15.89.247"
+PORT = 9559
+
+try:
+    print "try"
+    motionC = ALProxy("ALMotion", IPC, PORT)
+    
+except Exception,e:
+    print "Error when creating STMOBJECT_NAME proxy:"
+    print str(e)
+    exit(1)
+    
 
 from naoqi import ALProxy
 
 tts = ALProxy("ALTextToSpeech", "10.15.89.247", 9559)
-tts.say("la programación es una herramienta")
+tts.say("Hello, world!")
