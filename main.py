9004f526670b048b0e6c2ad30a3086aef4dbd5d6
Juan Nolazco <carlos.bcaceres@hotmail.com>
2016-09-28 15:38:45 -0500

    shoulder pitch

--- main.py
+++ main.py
@@ -35,31 +35,11 @@ def main(robotIP):
     StiffnessOn(motionProxy)
 
     # Send NAO to Pose Init
-    # postureProxy.goToPosture("Sit", 0.5)
+    postureProxy.goToPosture("StandInit", 0.5)
 
-    effector   = "LArm"
-    space      = motion.FRAME_ROBOT
-    axisMask   = almath.AXIS_MASK_VEL    # just control position
-    isAbsolute = False
+    
 
-    # Since we are in relative, the current position is zero
-    currentPos = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
-    # Define the changes relative to the current position
-    dx         =  0.00      # translation axis X (meters)
-    dy         =  0.00      # translation axis Y (meters)
-    dz         =  0.00      # translation axis Z (meters)
-    dwx        =  0.04      # rotation axis X (radians)
-    dwy        =  0.00      # rotation axis Y (radians)
-    dwz        =  0.00      # rotation axis Z (radians)
-    targetPos  = [dx, dy, dz, dwx, dwy, dwz]
-
-    # Go to the target and back again
-    path       = [targetPos, currentPos]
-    times      = [2.0, 4.0] # seconds
-
-    motionProxy.positionInterpolation(effector, space, path,
-                                      axisMask, times, isAbsolute)
+    motionProxy.angleInterpolation()
     
     time.sleep(1)
  #   postureProxy.goToPosture("Sit")
