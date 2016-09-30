14d4f206635951da9c1a7f2c78a94ded95a1240c
Daniel Leyva <leyva.danielr@gmail.com>
2016-09-30 10:40:19 -0500

    ascii art y cambio de color a la impresion de terminal

--- say.py
+++ say.py
@@ -7,7 +7,15 @@ say -- Say a text and print it in terminal.
 import time
 import random
 import sys
-
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def say(text, tts):
     """Make Nao say text and also print it in the terminal in a hacker fashion.
@@ -19,10 +27,25 @@ def say(text, tts):
     tts.say(text)
 
     typing_speed = 95
+    ascii_art = """
+==================================================================
+      ___       ___           ___           ___           ___     
+     /\__\     /\  \         |\__\         /\__\         /\  \    
+    /:/  /    /::\  \        |:|  |       /:/  /        /::\  \   
+   /:/  /    /:/\:\  \       |:|  |      /:/  /        /:/\:\  \  
+  /:/  /    /::\~\:\  \      |:|__|__   /:/__/  ___   /::\~\:\  \ 
+ /:/__/    /:/\:\ \:\__\     /::::\__\  |:|  | /\__\ /:/\:\ \:\__\ 
+ \:\  \    \:\~\:\ \/__/    /:/~~/~     |:|  |/:/  / \/__\:\/:/  /
+  \:\  \    \:\ \:\__\     /:/  /       |:|__/:/  /       \::/  / 
+   \:\  \    \:\ \/__/     \/__/         \::::/__/        /:/  /  
+    \:\__\    \:\__\                      ~~~~           /:/  /   
+     \/__/     \/__/                                     \/__/    
 
+==================================================================="""
+
+    print bcolors.BOLD+ bcolors.OKGREEN + ascii_art
     for word in text:
-        sys.stdout.write(word)
+        sys.stdout.write(bcolors.BOLD + bcolors.OKGREEN + word)
         sys.stdout.flush()
         time.sleep(random.random()*10.0/typing_speed)
-
-    print ''
+    print bcolors.ENDC + ''
