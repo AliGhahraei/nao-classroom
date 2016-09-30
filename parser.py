501fd01c98177673ef2c231f500029d89a28c96c
Ali Ghahraei Figueroa <aligf94@gmail.com>
2016-09-30 06:50:33 -0500

    Document and correct style of parser

--- parser.py
+++ parser.py
@@ -1,17 +1,31 @@
-#!/usr/bin/python
+"""String parsing and matching utilities.
+
+Functions:
+parse -- parse lines in exercise file and return wrong line or None.
+"""
+
 
 def parse(fileToParse, linesToParse, stringsToParse):
-	fileText = open(fileToParse).readlines()
-	
-	for lineToParse, stringToParse in zip(linesToParse, stringsToParse):
-		stringToParse = stringToParse.replace(' ','')
-		stringToParse = stringToParse.replace('\n', '')
-		
-		stringParsed = fileText[lineToParse-1]
-		stringParsed = stringParsed.replace('\n', '')
-		stringParsed = stringParsed.replace(' ','')
-		
-		if(stringParsed != stringToParse):
-			return lineToParse
-		
-	return None
+    """parse lines in exercise file, match list and return line differences.
+
+    Keyword arguments
+    fileToParse -- the complete path to the filename to parse.
+    linesToParse -- a list of line numbers to parse and check.
+    stringsToParse -- a list of the values that linesToParse must match.
+    """
+    fileText = open(fileToParse).readlines()
+
+    for lineToParse, stringToParse in zip(linesToParse, stringsToParse):
+        stringToParse = stringToParse.replace(' ', '')
+        stringToParse = stringToParse.replace('\n', '')
+
+        stringParsed = fileText[lineToParse-1]
+        stringParsed = stringParsed.replace('\n', '')
+        stringParsed = stringParsed.replace(' ', '')
+
+        # If a line to check differs, return its line number
+        if(stringParsed != stringToParse):
+            return lineToParse
+
+    # Reached if all lines match
+    return None
