b2addc724a35c3859e8982ddecff180b6e2ec9df
Ali Ghahraei Figueroa <aligf94@gmail.com>
2016-09-30 01:18:01 -0500

    Correct style of wheels exercise

--- exercises/control_movement.py
+++ exercises/control_movement.py
@@ -17,15 +17,15 @@ finch = Finch()
 # duration of their movement
 
 # CODE: rueda_izquierda = 0.5, rueda_derecha = 0, tiempo = 5
-rueda_izquierda = 
-rueda_derecha = 
-tiempo = 
+rueda_izquierda =
+rueda_derecha =
+tiempo =
 
 ###############################################################################
 
 # Ahora pide al usuario que ingrese las velocidades
-#rueda_izquierda = raw_input("Velocidad izquierda: ")
-#rueda_derecha = raw_input("Velocidad derecha: ")
+# rueda_izquierda = raw_input("Velocidad izquierda: ")
+# rueda_derecha = raw_input("Velocidad derecha: ")
 
 finch.wheels(rueda_izquierda, rueda_derecha)
 sleep(tiempo)
